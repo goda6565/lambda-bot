e5a257fc6cd6aacb950180408e71cdda5b093fa7
harutin <godaharuto@gmail.com>
2025-05-24 22:05:22 +0900

    機能追加: Slackボットのメンションイベントに対する返信メッセージをデバッグ情報に変更しました。また、Secrets Managerの新しいモジュールを追加し、関連する初期化ファイルを作成しました。

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from slack_bolt import App
 from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 
-from app.infra.secrets_manager.get_secret import get_secret
+from app.infrastructure.secrets_manager.get_secret import get_secret
 from app.config import setting
 
 app = App(
@@ -17,13 +17,11 @@ def lambda_handler(event, context):
 
 @app.event("app_mention")
 def handle_app_mention_events(event, say):
-    # メンションされたユーザーのIDを取得
-    user = event.get("user")
     # メッセージのタイムスタンプを取得（スレッドの親メッセージIDとして使用）
     ts = event.get("ts")
     
     # 返信メッセージを作成
-    response = f"<@{user}> さん、忙しいのでメンションしないでもらっていいですか？"
+    response = f"デバック: {event}"
     
     # スレッドに返信
     say(
