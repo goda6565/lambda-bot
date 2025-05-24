5c87f45188edf04d19e42329848cfee63efa7339
harutin <godaharuto@gmail.com>
2025-05-24 14:54:56 +0900

    Lambda関数の名前を変数からローカル変数に変更し、API Gatewayからの呼び出しを許可する設定を更新しました。

--- terraform/modules/api_gateway/aws_lambda_permission.tf
+++ terraform/modules/api_gateway/aws_lambda_permission.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_permission" "allow_apigateway" {
   statement_id  = "AllowInvokeFromApiGateway"
   action        = "lambda:InvokeFunction"
-  function_name = var.lambda_function_name
+  function_name = local.lambda_function_full_name
   qualifier     = var.lambda_alias_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
