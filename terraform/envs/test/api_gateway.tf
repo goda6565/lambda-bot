8d959aaf6e2948c831a68d8f65822fd63cec86db
harutin <godaharuto@gmail.com>
2025-05-25 10:36:21 +0900

    削除: テスト環境のTerraform設定からAPI Gateway、ECR、Lambda、Secrets Managerのモジュールを削除しました。

--- terraform/envs/test/api_gateway.tf
+++
@@ -1,7 +0,0 @@
-module "api_gateway" {
-  source                  = "../../modules/api_gateway"
-  env                     = var.env
-  lambda_function_name    = var.lambda_function_name
-  lambda_alias_name       = module.lambda.aws_lambda_alias_name
-  lambda_alias_invoke_arn = module.lambda.aws_lambda_alias_invoke_arn
-}
--- terraform/envs/test/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "api_gateway_invoke_url" {
-  value = module.api_gateway.api_gateway_invoke_url
-}
